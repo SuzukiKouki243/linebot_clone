b738889e013e779747ecf369069f74035a8238c2
SuzukiKouki243 <78122628+SuzukiKouki243@users.noreply.github.com>
2021-02-03 12:04:05 +0900

    Update demo.js

--- examples/demo.js
+++ examples/demo.js
@@ -19,15 +19,7 @@ bot.on('message', function (event) {
           console.log('**channelAccessToken=', event.source.channelAccessToken);
           
           const line = require('@line/bot-sdk');
-          const client = new line.Client({
-             channelAccessToken: '<channel access token>'
-          });
-          client.getLinkToken(event.source.userId)
-          
-          const client = new event.Client({
-            channelAccessToken: event.source.channelAccessToken
-          });
-          client.getLinkToken(event.source.userId)
+     
           
           event.reply({
             type: 'template',
